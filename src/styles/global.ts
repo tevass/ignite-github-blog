49441868ca31f0b89fa79ba84bd12e17ce3c13dc
Estevão <estevao.biondi@gmail.com>
2022-09-17 10:27:51 -0300

    initial commit

---
+++ src/styles/global.ts
@@ -0,0 +1,30 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme.colors.blue};
+  }
+
+  html, body, #root {
+    width: 100%;
+    height: 100%;
+  }
+
+  body {
+    background: ${(props) => props.theme.colors['base-background']};
+    color: ${(props) => props.theme.colors['base-text']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: Nunito, sans-serif;
+    line-height: 160%;
+  }
+`
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,25 @@
+export const defaultTheme = {
+  colors: {
+    blue: '#3294F8',
+    'base-title': '#E7EDF4',
+    'base-subtitle': '#C4D4E3',
+    'base-text': '#AFC2D4',
+    'base-span': '#7B96B2',
+    'base-label': '#3A536B',
+    'base-border': '#1C2F41',
+    'base-post': '#112131',
+    'base-profile': '#0B1B2B',
+    'base-background': '#071422',
+    'base-input': '#040F1A',
+  },
+  fonts: {
+    'title-lg': 'bold normal 1.5rem',
+    'title-md': 'bold normal 1.25rem',
+    'title-sm': 'bold normal 1.125rem',
+
+    'text-md': 'regular normal 1rem',
+    'text-sm': 'regular normal 0.875rem',
+
+    link: 'bold regular 0.75rem',
+  },
+} as const
